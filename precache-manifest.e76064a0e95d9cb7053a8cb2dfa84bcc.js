7d92e32ef085df53dae5893a4b92cb662c012cab
cruse1914 <contact.dm07@gmail.com>
2021-03-24 17:51:57 +0530

    Updates

---
+++ precache-manifest.e76064a0e95d9cb7053a8cb2dfa84bcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d18168964b0cf2dbdb2dbac710d6169",
+    "url": "/Corona-Tracker-App/index.html"
+  },
+  {
+    "revision": "9ceeb13e0279d64b1554",
+    "url": "/Corona-Tracker-App/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "119f740771bf88ac2c06",
+    "url": "/Corona-Tracker-App/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "9ceeb13e0279d64b1554",
+    "url": "/Corona-Tracker-App/static/js/2.d84326f2.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Corona-Tracker-App/static/js/2.d84326f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119f740771bf88ac2c06",
+    "url": "/Corona-Tracker-App/static/js/main.283b893a.chunk.js"
+  },
+  {
+    "revision": "dd490798037167e669b5",
+    "url": "/Corona-Tracker-App/static/js/runtime-main.732a98d9.js"
+  }
+]);
